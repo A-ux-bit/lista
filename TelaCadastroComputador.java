026433f8ed93e24ea2862b7a68f898a13c0dcff7
Alex.jp <61752020+A-ux-bit@users.noreply.github.com>
2023-09-11 16:51:52 -0300

    Update TelaCadastroComputador.java

--- TelaCadastroComputador.java
+++ TelaCadastroComputador.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.sql.Date;
 public class TelaCadastroComputador extends TelaCadastro {
     private String campoCpu;
     private String campoRam;
@@ -24,14 +24,32 @@ public class TelaCadastroComputador extends TelaCadastro {
 
     public void desenhaTelacadastroComputador(){
         Scanner inputComputador = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Código.......");
+        this.setId(inputComputador.nextLine());
+        System.out.print("Nome.......");
+        this.setNome(inputComputador.nextLine());
+        System.out.print("Modelo.......");
+        this.setModelo(inputComputador.nextLine());
+        System.out.print("Fornecedor.......");
+        this.setFornecedor(inputComputador.nextLine());
+        //System.out.print("Data de aquisição.......");
+        //this.setDataAquisicao(String.inputComputador.nextLine());
         System.out.print("C.P.U.......");
         this.campoCpu = inputComputador.nextLine();
         System.out.print("R.A.M.....");
         this.campoRam = inputComputador.nextLine();
         System.out.print("Software.....");
         this.campoSoftware = inputComputador.nextLine();
-        System.out.println(this.getCampoCpu()+this.getCampoRam()+this.getCampoSoftware());
+        System.out.print("Ativo [S/N].......");
+        
+        if (input.nextLine().equals("S")){
+            this.setAtivo(true);
+        }
+        else if(input.nextLine().equals("N")) {
+            this.setAtivo(false);
+        }
 
         //inputComputador.close();
     }
