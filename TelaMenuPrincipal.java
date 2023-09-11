e7de3a65b9bab9f2cd67f7e5f476517ec2f68878
Alex.jp <61752020+A-ux-bit@users.noreply.github.com>
2023-09-11 16:51:16 -0300

    Update TelaMenuPrincipal.java

--- TelaMenuPrincipal.java
+++ TelaMenuPrincipal.java
@@ -2,7 +2,7 @@ public class TelaMenuPrincipal{
     public TelaMenuPrincipal(){
      System.out.println("*******ESCCOLHA UMA OPÇAO****** \n \n"+ 
       "[1] cadastro computador\n"+
-      "[2] cadastro equipamentoz\n"+
+      "[2] Listar cadastro\n"+
       "[3] sair\n"+
       "Escolhe a opção");
                    
