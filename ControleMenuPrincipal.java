a6d3c41146be62d37ab0e22637d02f47034e131a
Alex.jp <61752020+A-ux-bit@users.noreply.github.com>
2023-09-11 16:50:48 -0300

    Update ControleMenuPrincipal.java

--- ControleMenuPrincipal.java
+++ ControleMenuPrincipal.java
@@ -1,24 +1,39 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ControleMenuPrincipal {
+
     public ControleMenuPrincipal (){
          Scanner input = new Scanner(System.in);
+         ControleCadastroComputador controleCadastroComputador = new ControleCadastroComputador();
          {
             new TelaMenuPrincipal();
-             switch (Integer.parseInt(input.nextLine())) {
+           // ControleCadastroComputador controleCadastroComputador = new ControleCadastroComputador();
+
+            switch (Integer.parseInt(input.nextLine())) {
             case 1:
-            new ControleCadastroComputador();
+            controleCadastroComputador.cadastroComputador();
+            controleCadastroComputador.getArrayComputador().forEach((comp) -> System.out.println(comp));
+            new ControleMenuPrincipal();
+           //controleCadastroComputador.getArrayComputador().foreah((comp))
             break;
+
             case 2:
-                 System.out.println("Em construção.....");
+            //controleCadastroComputador.getArrayComputador().forEach((comp) -> System.out.println(comp));
+            //new ControleMenuPrincipal();
+            break;
             case 3:
             System.exit(0);
             break;
         
             default:
+            new ControleMenuPrincipal();
                 break;
         }
         
     }
     
     }}
+   
+
